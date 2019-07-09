b63ad7c359e0fa195c4d031759ea46cd091b45cd
weizhanzhan <375786117@qq.com>
2019-07-09 15:27:17 +0800

    js下拉刷新

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -48,7 +48,7 @@
             </ul>
           </div>
         </div>
-        <Aside :no-header-top="noHeaderTop" />
+        <!-- <Aside :no-header-top="noHeaderTop" /> -->
       </div>
     </div>
   </div>
@@ -68,6 +68,7 @@
         pageSize: 10,
         currentPage: 1,
         totalPage: 1,
+        count: 1,
         blogs: [],
         searchBlog: ''
       }
@@ -77,6 +78,54 @@
         return this.blogs.filter(blog => {
           return blog.title.match(this.searchBlog)
         })
+      }
+    },
+    mounted() {
+      // 滚动条在Y轴上的滚动距离
+      function getScrollTop() {
+        var scrollTop = 0; var bodyScrollTop = 0; var documentScrollTop = 0
+        if (document.body) {
+          bodyScrollTop = document.body.scrollTop
+        }
+        if (document.documentElement) {
+          documentScrollTop = document.documentElement.scrollTop
+        }
+        scrollTop = (bodyScrollTop - documentScrollTop > 0) ? bodyScrollTop : documentScrollTop
+        return scrollTop
+      }
+
+      // 文档的总高度
+
+      function getScrollHeight() {
+        var scrollHeight = 0; var bodyScrollHeight = 0; var documentScrollHeight = 0
+        if (document.body) {
+          bodyScrollHeight = document.body.scrollHeight
+        }
+        if (document.documentElement) {
+          documentScrollHeight = document.documentElement.scrollHeight
+        }
+        scrollHeight = (bodyScrollHeight - documentScrollHeight > 0) ? bodyScrollHeight : documentScrollHeight
+        return scrollHeight
+      }
+
+      // 浏览器视口的高度
+
+      function getWindowHeight() {
+        var windowHeight = 0
+        if (document.compatMode === 'CSS1Compat') {
+          windowHeight = document.documentElement.clientHeight
+        } else {
+          windowHeight = document.body.clientHeight
+        }
+        return windowHeight
+      }
+      var _this = this
+      window.onscroll = function() {
+        if (getScrollTop() + getWindowHeight() === getScrollHeight()) {
+          _this.pageSize += 10
+          _this.init()
+          console.log('已经到最底部了！!')
+        }
       }
     },
     methods: {
